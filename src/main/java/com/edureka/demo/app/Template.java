e85f3a1df913080b34e9150126679fdf90f944c7
tejas <tejaswibv@gmail.com>
2020-07-26 20:11:06 +0000

    committing all

---
+++ src/main/java/com/edureka/demo/app/Template.java
@@ -0,0 +1,7 @@
+package com.edureka.demo.app;
+
+public class Template {
+	public static void main(String args[]) {
+		System.out.println("main class");
+	}
+}
---
+++ src/test/java/com/edureka/demo/app/TemplateTest.java
@@ -0,0 +1,13 @@
+package com.edureka.demo.app;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class TemplateTest {
+	@Test
+	public void doTest() {
+		Template temp = null;
+		//assertNotNull(temp);
+	}
+}
