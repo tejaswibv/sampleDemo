1d8ab39dbc3eba6bb3c685c96ba9397bd9846c2e
xyz <labuser@ip-172-31-20-15.ap-south-1.compute.internal>
2021-11-30 04:24:28 +0000

    added a new method

--- src/main/java/com/edureka/demo/app/Template.java
+++ src/main/java/com/edureka/demo/app/Template.java
@@ -4,4 +4,8 @@ public class Template {
 	public static void main(String args[]) {
 		System.out.println("main class");
 	}
+	
+	public void newMethod() {
+		System.out.print("this is a new method");
+	}
 }
